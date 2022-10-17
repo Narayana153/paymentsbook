7d8ce2984985f093db614c5163b27c2652aec4a1
Narayana153 <95913215+Narayana153@users.noreply.github.com>
2022-10-17 06:40:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1464,7 +1464,7 @@ def logout():
         return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4444, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 
